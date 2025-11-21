446c17da6b7bac161a8b9445e95a9c113245c63a
Kiheon Kwak <58392517+GreenKronic@users.noreply.github.com>
2025-11-21 15:42:34 +0900

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 VAR="$1"
+PIPE="/var/run/hostbrowserpipe"
 
 if [ -z "$VAR" ]; then
   echo "No argument provided"
@@ -11,3 +12,22 @@ cd /opt/darknet/
 wget $VAR -O detect.jpg || { echo "Failed to download file"; exit 2; }
 
 ./darknet detect cfg/yolov3.cfg yolov3.weights detect.jpg || echo "Darknet detection failed"
+
+mv predictions.jpg /vol || { echo "Failed to move predictions.jpg"; }
+
+if [ -z "$HOST_DIR" ]; then
+  echo "no VOLUME mounted, skipping browser open."
+  exit 0
+fi
+
+URL="file://$HOST_DIR/predictions.jpg"
+
+
+if [ -p "$PIPE" ]; then
+  if ! timeout 1 bash -c "echo \"$URL\" > \"$PIPE\""; then
+    echo "Pipe exists but no reader available, skipping open."
+  fi
+else
+  echo "Host browser pipe not found, skipping."
+fi
+
---
+++ hw1.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+VAR="$1"
+PIPE="$PWD/hostbrowserpipe"
+dir="$PWD"
+
+rm -rf "$PIPE"         
+mkfifo "$PIPE"
+
+# Background pipe listener
+(
+  while read -r URL; do
+    echo "Opening: $URL"
+    xdg-open "$URL"
+  done <"$PIPE"
+) &
+
+LISTENER_PID=$!
+echo "Listener PID: $LISTENER_PID"
+
+
+
+cleanup() {
+  echo "Cleaning up..."
+  kill "$LISTENER_PID" 2>/dev/null || true
+  rm -rf "$PIPE" "predictions.jpg"
+}
+trap cleanup EXIT
+
+
+
+# Docker run 
+docker run -it --rm \
+  -v "$PIPE":/var/run/hostbrowserpipe \
+  -v "$dir":/vol \
+  -e HOST_DIR="$dir" \
+  hw1 \
+  "$VAR"
+
+while true; do
+  sleep 1
+done
+# docker run -it hw1 https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg
